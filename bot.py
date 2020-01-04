e92e9c60e529ccdb9742afade3c9f8f97b3f4f73
Ethan Bonnardeaux <ethanbonn21@gmail.com>
2020-01-04 15:45:59 -0500

    like function tested

--- bot.py
+++ bot.py
@@ -3,10 +3,14 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import  WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
-user_name_global = raw_input()
+'''user_name_global = raw_input()
 p_word_global = raw_input()
-hashtag_global = raw_input()
+hashtag_global = raw_input()'''
+
 
 
 chrome_options = Options()
@@ -23,33 +27,41 @@ class Insta:
     def login(self):
         bot = self.bot
         bot.get("https://www.instagram.com/accounts/login/?source=auth_switcher")
-        time.sleep(3)
-        email = bot.find_element_by_name("username")
+        email =  WebDriverWait(bot, 10).until( EC.presence_of_element_located((By.NAME, "username")))
         password = bot.find_element_by_name("password")
         email.clear()
         password.clear()
         email.send_keys(self.username)
         password.send_keys(self.password)
         password.send_keys(Keys.RETURN)
-        time.sleep(1)
-        #asks about notifications
-        notifs = bot.find_element_by_class_name("HoLwm")
+        notifs = WebDriverWait(bot, 10).until( EC.presence_of_element_located((By.CLASS_NAME, "HoLwm")))
         notifs.send_keys(Keys.RETURN)
 
     def like_post(self):
         bot = self.bot
         bot.get("https://www.instagram.com/explore/tags/" + self.hashtag)
-        time.sleep(3)
-        for i in range(3):
+        WebDriverWait(bot, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "o64aR")))
+
+        '''for i in range(6):      #gets 216 episodes
             bot.execute_script('window.scrollTo(0, document.body.scrollHeight)')
-            time.sleep(1)
+            if i % 3 == 0:
+                time.sleep(3) # waiting for new page to load
+            #test this!!!!
+        '''
+
         post_class = bot.find_elements_by_class_name("v1Nh3")
-        for i in range(50):
-            bot.get(post_class[i].find_element_by_tag_name("a").get_attribute("href"))
+
+        links = []
+        for i in range(10):     #getting all the links
+            links.append(post_class[i].find_element_by_tag_name("a").get_attribute("href"))
+
+        for j in range(10):
+            time.sleep(1)
+            bot.get(links[j])
             time.sleep(1)
             bot.find_element_by_class_name("fr66n").find_element_by_tag_name("button").click()
 
 
-account = Insta(user_name_global, p_word_global, hashtag_global)
+account = Insta("put you user name here", "password", "hashtag")
 account.login()
 account.like_post()
